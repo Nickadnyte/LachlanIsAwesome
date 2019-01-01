d3fe8578b636150a25091dea6739cd19f26d86c2
Lachlan Li <0654412@schoolsnet.act.edu.au>
2019-01-01 16:23:01 +1100

    improve dads program. Making it much faster

--- writeLachlanIsAwesome.py
+++ writeLachlanIsAwesome.py
@@ -1,16 +1,13 @@
 # writeLachlanIsAwesome.py
 # License see License.txt
 
-# Import python's library for finding text with regular expressions
-import re
 
 # Write to LachlanIsAwesome.txt
 f = open("LachlanIsAwesome.txt", "w")
 
 # Use a while loop, write "Lachlan is awesome 10 times
 counter = 0;
-while counter != 10:
+while counter != 1000000:
     f.write("Lachlan is awesome!\n")
     counter+=1
-    print ("Wrote \"Lachlan is awesome!\" " + str(counter) + " times.")
 
